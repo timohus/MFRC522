34987a76679689646548f55579b77bb83ee26198
Tim Samoilenko <timohus@gmail.com>
2024-04-02 21:12:20 +0200

    Update SimpleMFRC522.py include device id in init

--- mfrc522/SimpleMFRC522.py
+++ mfrc522/SimpleMFRC522.py
@@ -10,8 +10,8 @@ class SimpleMFRC522:
   KEY = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
   BLOCK_ADDRS = [8, 9, 10]
   
-  def __init__(self):
-    self.READER = MFRC522()
+  def __init__(self, device=0):
+    self.READER = MFRC522(device=device)
   
   def read(self):
       id, text = self.read_no_block()
